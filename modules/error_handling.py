f47c72b369032895f3903cd197180f6e7ef39c6c
MoritzBlumer <lmb215@cam.ac.uk>
2024-10-11 14:05:10 +0100

    remove error_handling.py

--- modules/error_handling.py
+++
@@ -1,6 +0,0 @@
-
-
-# sanity checks:
-# - is group a column in metadata
-# - if hex_codes: are existingall values from group included, are all hex codes real
-# - if sequence/sequences: check if they exist in data
