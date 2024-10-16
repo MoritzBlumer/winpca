d3ead6edf6fe77ba6c24f3b0f543e9d7759dee88
MoritzBlumer <lmb215@cam.ac.uk>
2024-10-16 13:43:34 +0100

    testing

--- modules/windowed_pca.py
+++ modules/windowed_pca.py
@@ -472,7 +472,9 @@ class WPCA:
                         pos = int(line[1])
                         gts = [line[9:][idx].split(':')[0] for idx in sample_idx_lst]
                         gts = [self.gt_code_dct[x] for x in gts]
-                        if self.skip_monomorphic and len(set(gts)) == 1: continue
+                        if self.skip_monomorphic and len(set(gts)) == 1: 
+                            print(f'skipped {pos}')
+                            continue
                         while self.w_stop < pos:
                             if self.win: self.gt_process_win()
                             if self.stop < self.w_stop: break
