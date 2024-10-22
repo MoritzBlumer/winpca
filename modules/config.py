74a51f6c7884caa81a5b5b77ceabd76a32d8c75f
MoritzBlumer <lmb215@cam.ac.uk>
2024-10-22 13:52:25 +0100

    update defaults

--- modules/config.py
+++ modules/config.py
@@ -31,8 +31,7 @@ PLOT_INTERVAL = None       # plot only every nth value (10th if specifying 10)
 #  (these can only be changed here, i.e. no CLI arguments)
 
 # pca
-MIN_VAR_PER_W = 20        # min # of variants per window, otherwise empty output
-GT_MIN_VAR_PER_W = 25     # min # of variants per window, otherwise empty output
+GT_MIN_VAR_PER_W = 20     # min # of variants per window, otherwise empty output
 GL_PL_MIN_VAR_PER_W = 100 # min # of variants per window, otherwise empty output
 SKIP_MONOMORPHIC = True   # skip invariant sites
 VCF_PASS_FILTER = True    # include only sites where FILTER is set to PASS
